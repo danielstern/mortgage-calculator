ad73a2993a03227fba64d2381841b9aa961a7acc
daniel stern <daniel@danielstern.ca>
2013-10-10 09:34:27 -0400

    so far directivizing is good

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 define(['angular', 'jquery', 'css!common/styles/bootstrapcalculator'], function (angular, $) {
 
   return angular.module('calculatorApp', [])
+  .directive('rcChart', function () {
+    return {
+      restrict: 'A',
+      scope: {
+      	kind: '=',
+      },
+      link: function (scope, elem, attrs) {
+        console.log("Recognized rc Chart",scope.kind);
+
+      },
+    }
+  });
    
 
 });
--- charts/ChartCtrl.js
+++ charts/ChartCtrl.js
@@ -1,7 +1,7 @@
 define(['app',] , function (app) {
  	app.controller('ChartCtrl', ['$scope', 'chartService', function($scope, chartService) {
 
- 		$scope.charts = [1,2,3]
+ 		$scope.charts = [{kind:'bars'},{kind:'donut'},{kind:'stacked-bars'}]
  		
  		_.defer(function () {
  		  $('.chart-container > *').click(function (e) {
