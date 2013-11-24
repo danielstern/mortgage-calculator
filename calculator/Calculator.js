01a1e01983c0c0036b80459815163d8e8084dffe
unknown <daniel@danielstern.ca>
2013-11-24 16:56:27 -0500

    calc outputs, but not so accurately

--- calculator/Calculator.js
+++ calculator/Calculator.js
@@ -22,11 +22,11 @@ define(['underscore'], function (_) {
     calc.preflightAndGo = function(params) {
      // console.log("Preflighting...",params);
       var amortizationWeeks = params.amortization * 12;
-      var paymentFreq = 'monthly';
+      var paymentFreq = 'weekly';
       if (params.paymentBiWeekly) paymentFreq = 'biWeekly';
       if (params.paymentMonthly) paymentFreq = 'monthly';
 
-      var interestRatePercent = 1 + (params.interestRate / 100)
+      var interestRateMonthly = 1 + ((params.interestRate / 100) / 12)
 
       var downPayment;
       if(params.downpayPercentSelected) {
@@ -35,10 +35,10 @@ define(['underscore'], function (_) {
         downPayment = params.downpay;
       }
 
-      calc.calculateMortgage(params.investmentValue, downPayment,interestRatePercent, amortizationWeeks, paymentFreq);
+      calc.calculateMortgage(params.investmentValue, downPayment,interestRateMonthly, amortizationWeeks, paymentFreq);
     }
 
-    calc.calculateMortgage = function(p1, dp, i, pm, pfq) {
+    calc.calculateMortgage = function(p1, dp, pi, pm, pfq) {
        console.log("Mortgage go!",arguments);
        var r = {};
 
@@ -48,7 +48,70 @@ define(['underscore'], function (_) {
        r.dpp = dp / p1;
        console.log("r", r);
 
+      var precision;
+      var targetPrecision = 100; // less than this amount apart  
+
+      var count = 1000;
+      var adjustmentAmount = 50;
+
+      console.log("PV?",pv);
+       var pve = pv;
+
        // phase 2;
+       // guess at mw;
+       var gmw = pv / 100;
+       while(count > 0) {
+    //  console.log("Gmw?", gmw);
+         for (i = 0; i < pm ; i++) {
+         // console.log("PVE?",pve);
+         // console.log("week passes...");
+          if (i == 0 || i%4 == 0) {
+            if (pfq == "monthly") pve = pve - gmw;
+            pve *= pi;
+
+          }
+
+         // console.log("PVE afer interest?",pve, i);
+
+          if (i == 0 || i%2 == 0) {
+            if (pfq == "biWeekly") pve = pve - gmw;
+          }
+
+          if (pfq == "weekly") pve = pve - gmw;
+
+        //  console.log("Pve at end of this week?",pve);
+          if (pve < 0) break;
+
+       }
+
+    //   console.log("Our estimated value at end of term...",pve);
+
+        if(pve > 0) {
+
+          gmw += adjustmentAmount;
+
+        } else {
+
+          gmw -= adjustmentAmount;
+
+        }
+        precision = Math.abs(pve);
+
+        if(precision < targetPrecision) {
+          console.log("precision?",precision);
+          break;
+        }
+
+        adjustmentAmount *= 0.99;
+        pve = pv;
+
+       count --;
+
+     }
+
+    console.log("what is pve?",pve);
+    console.log("what is gmw?", gmw);
+
 
     }
   }
